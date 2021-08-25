a718225738be1d558af5f6d4dd3e768d44fb3e58
Michal-MK <michalhz159@gmail.com>
2021-08-25 20:01:48 +0200

    Styling - added style to the WPF app - improved naming of GivenBy as the Unity space constratint was lifted by ditching Unity lel

--- Bl2 Client-WPF/Colors.cs
+++ Bl2 Client-WPF/Colors.cs
@@ -3,17 +3,17 @@
 namespace Bl2Client {
 	public static class Colors {
 		public static readonly SolidColorBrush HAMMERLOCK = new(Color.FromRgb(10, 200, 240));
-		public static readonly SolidColorBrush BBOARD = new(Color.FromRgb(100, 100, 100));
+		public static readonly SolidColorBrush BOUNTY_BOARD = new(Color.FromRgb(100, 100, 100));
 		public static readonly SolidColorBrush LILITH = new(Color.FromRgb(230, 0, 130));
 		public static readonly SolidColorBrush MORDECAI = new(Color.FromRgb(255, 0, 0));
 		public static readonly SolidColorBrush SCOOTER = new(Color.FromRgb(50, 100, 0));
-		public static readonly SolidColorBrush TINYTINA = new(Color.FromRgb(220, 160, 0));
+		public static readonly SolidColorBrush TINY_TINA = new(Color.FromRgb(220, 160, 0));
 		public static readonly SolidColorBrush TANNIS = new(Color.FromRgb(130, 0, 200));
 		public static readonly SolidColorBrush BRICK = new(Color.FromRgb(100, 40, 20));
 		public static readonly SolidColorBrush ELLIE = new(Color.FromRgb(0, 25, 200));
 
 		public static SolidColorBrush GetColor(string name) {
-			return (SolidColorBrush)typeof(Colors).GetField(name.ToUpper())!.GetValue(null)!;
+			return (SolidColorBrush)typeof(Colors).GetField(name.ToUpper().Replace(' ', '_'))!.GetValue(null)!;
 		}
 	}
 }
