b5c92eae343293d322ffc2335c2c9ed870cc75aa
Michal-MK <michalhz159@gmail.com>
2021-10-17 11:22:31 +0200

    Added messageboxes with details what is wrong. - suppressed possible null dereference on connection

--- Bl2 Client-WPF/MainWindow.xaml.cs
+++ Bl2 Client-WPF/MainWindow.xaml.cs
@@ -23,18 +23,18 @@ namespace Bl2Client {
 		private async void OnConnect(object sender, RoutedEventArgs e) {
 
 			if (!IPAddress.TryParse(Hostname, out IPAddress? address)) {
-				// TODO Log
+				MessageBox.Show("Not a valid IP address. (Hostname resolution is not supported yet.)", "Error occurred");
 				return;
 			}
 			if (!ushort.TryParse(Port, out ushort port)) {
-				// TODO Log
+				MessageBox.Show("Not a valid port.", "Error occurred");
 				return;
 			}
 
 			connection = await Connection.Init(address, port);
 
 			if (connection == null) {
-				// TODO Log
+				MessageBox.Show($"Unable to connect to {Hostname}!", "Error occurred");
 				return;
 			}
 
@@ -44,15 +44,15 @@ namespace Bl2Client {
 		}
 
 		public void OnNewAvailableQuest(Quest q) {
-			connection.Client.Connection.SendData(Constants.PACKET_ID, new Packet(q.ID, QuestStatus.Accepted));
+			connection!.Client.Connection.SendData(Constants.PACKET_ID, new Packet(q.ID, QuestStatus.Accepted));
 		}
 
 		public void OnQuestTrigger(Quest q) {
-			connection.Client.Connection.SendData(Constants.PACKET_ID, new Packet(q.ID, QuestStatus.Triggered));
+			connection!.Client.Connection.SendData(Constants.PACKET_ID, new Packet(q.ID, QuestStatus.Triggered));
 		}
 
 		public void OnQuestDelete(Quest q) {
-			connection.Client.Connection.SendData(Constants.PACKET_ID, new Packet(q.ID, QuestStatus.Hidden));
+			connection!.Client.Connection.SendData(Constants.PACKET_ID, new Packet(q.ID, QuestStatus.Hidden));
 		}
 
 		private void OnFullSync(Quest[] obj) {
